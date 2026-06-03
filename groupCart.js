a49b309b861015aca917944eaa479a6f66819ed5
Muhammad Hamza Khan <hkdevelopment655@gmail.com>
2026-06-03 13:37:04 +0500

    Update groupCart.js

--- groupCart.js
+++ groupCart.js
@@ -1,35 +1,23 @@
 import express from 'express';
-import {
-  createGroupCart,
-  getGroupCart,
-  getUserGroupCarts,
-  addItemToGroupCart,
-  removeItemFromGroupCart,
-  joinGroupCart,
-  confirmGroupOrder,
-  deleteGroupCart,
-  getAllRegisteredUsers,
-  payMemberShare,
-  markMemberPaid,
-  getGroupMessages,
-  sendGroupMessage
-} from '../controllers/groupCartController.js';
-import { protect } from '../middleware/auth.js';
+import { 
+  createOrder, 
+  getOrders, 
+  getOrderById, 
+  initiatePayFastPayment, 
+  verifyPayment 
+} from '../controllers/paymentController.js';
 
 const router = express.Router();
-router.get('/users/all', protect, getAllRegisteredUsers);
-router.post('/', protect, createGroupCart);
-router.get('/', protect, getUserGroupCarts);
-router.get('/join/:shareLink', protect, joinGroupCart);
-router.get('/:id', protect, getGroupCart);
-router.post('/:id/items', protect, addItemToGroupCart);
-router.delete('/:id/items/:itemId', protect, removeItemFromGroupCart);
-router.delete('/:id', protect, deleteGroupCart);
-router.post('/:id/confirm', protect, confirmGroupOrder);
-router.post('/:id/pay-share', protect, payMemberShare);
-router.post('/:id/members/:memberId/pay', protect, markMemberPaid);
-router.patch('/:id/members/:memberId/paid', protect, markMemberPaid);
-router.get('/:id/messages', protect, getGroupMessages);
-router.post('/:id/messages', protect, sendGroupMessage);
+
+router.route('/')
+  .post(createOrder)
+  .get(getOrders);
+
+router.route('/:id')
+  .get(getOrderById);
+
+// PayFast Endpoints
+router.post('/initiate', initiatePayFastPayment);
+router.post('/verify', verifyPayment);
 
 export default router;
